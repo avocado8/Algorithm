e6bdc3f47fb68075b9d9c4eeb81dc3d202739b40
avocado8 <95484237+avocado8@users.noreply.github.com>
2024-03-04 19:51:58 +0900

    [Gold IV] Title: 검문, Time: 580 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/2981. 검문/검문.cc
+++ 백준/Gold/2981. 검문/검문.cc
@@ -8,8 +8,6 @@
 using namespace std;
 
 int gcdIter(int a, int b) {
-	if (a < b) swap(a, b);
-
 	while (b) {
 		a %= b;
 		swap(a, b);
@@ -17,13 +15,28 @@ int gcdIter(int a, int b) {
 	return a;
 }
 
-vector<int> factor(int a) {
+vector<int> makeDiffVec(vector<int>num) {
+	int n = num.size();
+	vector<int>diff(n - 1);
+	for (int i = 1; i <= n - 1; i++) {
+		diff[i - 1] = abs(num[i] - num[i - 1]);
+	}
+	return diff;
+}
+
+vector<int> findNums(vector<int>diff) {
 	vector<int>res;
-	for (int i = 2; i <= (a/2); i++) {
-		if (a % i == 0) {
+	int gcd = diff[0];
+	for (int i = 1; i < diff.size(); i++) {
+		gcd = gcdIter(gcd, diff[i]);
+	}
+
+	for (int i = 2; i <= gcd / 2; i++) {
+		if (gcd % i == 0) {
 			res.push_back(i);
 		}
 	}
+	res.push_back(gcd);
 	return res;
 }
 
@@ -39,28 +52,9 @@ int main() {
 	for (int i = 0; i < n; i++) {
 		cin >> num[i];
 	}
-	for (int i = 1; i <= n - 1; i++) {
-		diff[i - 1] = abs(num[i] - num[i - 1]);
-	}
-
-	int gcd = diff[0];
-	vector<int>res;
-	if (diff.size() == 1) {
-		gcd = diff[0];
-	}
-	else {
-		for (int i = 1; i < diff.size(); i++) {
-			gcd = gcdIter(gcd, diff[i]);
-		}
-	}
-
-	for (int i = 2; i <= gcd/2; i++) {
-		if (gcd % i == 0) {
-			res.push_back(i);
-		}
-	}
-	res.push_back(gcd);
-
+	diff = makeDiffVec(num);
+	
+	vector<int>res = findNums(diff);
 	for (int i = 0; i < res.size(); i++) {
 		cout << res[i] << ' ';
 	}
