52addcb37c46986607d0bdc291f00f21f8566ae9
avocado8 <95484237+avocado8@users.noreply.github.com>
2024-03-07 14:11:31 +0900

    [Gold V] Title: 강의실 배정, Time: 60 ms, Memory: 6048 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.cc
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.cc
@@ -7,30 +7,33 @@
 using namespace std;
 typedef pair<int, int>ci;
 
-int arrayRoom(int n, vector<ci>lec) {
-	priority_queue<int, vector<int>, greater<>> pq;
-	pq.push(0);
-	//입력으로 들어오는 값은 시작시각 기준으로 정렬되어 있음
-	//우선순위큐에 저장할 값은 종료시각임
+int sugang(int n, vector<ci>subject) {
+	//시작 시간 오름차순 정렬
+	sort(subject.begin(), subject.end());
+	//큐에는 종료시각을 오름차순으로 저장
+	priority_queue<int, vector<int>, greater<>>pq;
+	pq.push(-1);
 	for (int i = 0; i < n; i++) {
-		if (lec[i].first >= pq.top()) {
-			//시작시간이 이전 종료시간보다 같거나 느림 -> 같은 강의실 사용 가능
+		if (subject[i].first >= pq.top()) {
+			//시작시각이 종료시각보다 늦다면 같은 강의실 사용가능
 			pq.pop();
 		}
-		pq.push(lec[i].second); //기존 강의실 정보 갱신 or 새로운 강의실 정보 저장
+		//종료시각 큐에 푸시
+		pq.push(subject[i].second);
 	}
 	return pq.size();
 }
 
 int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
 	int n;
 	cin >> n;
-	vector<ci>lec(n, ci(0, 0));
+	vector<ci>lec(n);
 	for (int i = 0; i < n; i++) {
 		cin >> lec[i].first >> lec[i].second;
 	}
-	sort(lec.begin(), lec.end());
-	cout << arrayRoom(n, lec) << '\n';
+	cout << sugang(n, lec);
+	
 	return 0;
 }
-
