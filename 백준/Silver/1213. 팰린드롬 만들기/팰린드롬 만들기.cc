b58e9bf60ac00795063c26cb110d107303143bcd
avocado8 <95484237+avocado8@users.noreply.github.com>
2024-03-20 19:23:28 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
@@ -2,44 +2,40 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <stack>
 
 using namespace std;
-
 const int SIZE = 26;
-vector<int> countAlp(string s) {
-	vector<int> count(SIZE, 0);
-	for (int i = 0; i < s.size(); i++) {
-		count[s[i] - 'A']++;
+
+vector<int> alpCount(string str) {
+	vector<int>cnt(SIZE, 0);
+	for (int i = 0; i < str.size(); i++) {
+		cnt[str[i] - 'A']++;
 	}
-	return count;
+	return cnt;
 }
 
-string makePalindrome(vector<int>& count) {
+string solution(string str) {
+	vector<int>cnt = alpCount(str);
 	string p1 = "", p2 = "", p3 = "";
 	for (int i = 0; i < SIZE; i++) {
-		if (count[i] % 2 == 1) {
-			if (p2.size() == 1) {
-				return "I'm Sorry Hansoo";
-			}
+		if (cnt[i] % 2 == 1) {
+			if (p2.size() == 1) return "I'm Sorry Hansoo";
 			p2 = (char)(i + 'A');
 		}
-		for (int j = 0; j < count[i] / 2; j++) {
-			p1 = p1 + (char)(i + 'A');
+		for (int j = 0; j < cnt[i] / 2; j++) {
+			p1 += (char)(i + 'A');
 			p3 = (char)(i + 'A') + p3;
 		}
 	}
 	return p1 + p2 + p3;
 }
 
-string solution(string s) {
-	vector<int>count = countAlp(s);
-	return makePalindrome(count);
-}
-
 int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
 	string s;
 	cin >> s;
 	cout << solution(s);
 	return 0;
 }
-
