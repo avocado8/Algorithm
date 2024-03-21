d2230f58312177b6a9b1f06eb78b0717b5ad33af
avocado8 <95484237+avocado8@users.noreply.github.com>
2024-03-21 19:51:03 +0900

    [Gold IV] Title: 스도쿠, Time: 72 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.cc
+++ 백준/Gold/2580. 스도쿠/스도쿠.cc
@@ -3,47 +3,49 @@
 
 using namespace std;
 const int MAX = 9;
-//check[a][b] -> a행/열/구역에 숫자 b가 존재함
-bool check_row[MAX][MAX + 1]; //각 행에서 중복 체크
-bool check_col[MAX][MAX + 1]; //각 열
-bool check_3x3[MAX][MAX + 1]; //각 3x3사각형
-int get3x3Idx(int row, int col) {
+
+bool check_row[MAX][MAX + 1];
+bool check_col[MAX][MAX + 1];
+bool check_3x3[MAX][MAX + 1];
+
+int get33Index(int row, int col) {
 	return (row / 3) * 3 + (col / 3);
 }
 
-bool fillSudoku(int idx, vector<vector<int>>* sudoku) {
+bool fillSudoku(int idx, vector<vector<int>>*sudoku) {
+	//종료조건
 	if (idx == MAX * MAX) return true;
 	int row = idx / MAX;
 	int col = idx % MAX;
-	if ((*sudoku)[row][col]) { //이미 채워진 칸이면 다음으로
-		return fillSudoku(idx + 1, sudoku);
+
+	if ((*sudoku)[row][col] != 0) { //이미 숫자가 채워진 칸이면
+		return fillSudoku(idx + 1, sudoku); //다음 칸으로 넘어감
 	}
+
 	for (int i = 1; i <= MAX; i++) {
-		//1~9까지 넣어보면서 조건 만족하나 확인
-		if (!check_row[row][i] && !check_col[col][i] && !check_3x3[get3x3Idx(row, col)][i]) {
-			//만족하면 해당칸에 숫자 들어갈 수 있음
-			//체크 배열에 표기
+		//아직 채워지지 않은 숫자라면
+		if (!check_row[row][i] && !check_col[col][i] && !check_3x3[get33Index(row, col)][i]) {
+			//칸 채우기
 			check_row[row][i] = true;
 			check_col[col][i] = true;
-			check_3x3[get3x3Idx(row, col)][i] = true;
-			//스도쿠 칸 채우기
+			check_3x3[get33Index(row,col)][i] = true;
 			(*sudoku)[row][col] = i;
-
+			//다음칸
 			if (fillSudoku(idx + 1, sudoku)) {
 				return true;
 			}
-
-			//체크 배열 다시 수정, 스도쿠 칸 비우기
+			//다시 비우기
 			check_row[row][i] = false;
 			check_col[col][i] = false;
-			check_3x3[get3x3Idx(row, col)][i] = false;
+			check_3x3[get33Index(row, col)][i] = false;
 			(*sudoku)[row][col] = 0;
 		}
 	}
+	//위에서 다 안됐으면... 안되는거
 	return false;
 }
 
-vector<vector<int>> solution(vector<vector<int>>sudoku) {
+vector<vector<int>> solution(vector<vector<int>> sudoku) {
 	vector<vector<int>>answer(MAX, vector<int>(MAX));
 	for (int i = 0; i < MAX; i++) {
 		for (int j = 0; j < MAX; j++) {
@@ -51,7 +53,7 @@ vector<vector<int>> solution(vector<vector<int>>sudoku) {
 			if (sudoku[i][j]) {
 				check_row[i][sudoku[i][j]] = true;
 				check_col[j][sudoku[i][j]] = true;
-				check_3x3[get3x3Idx(i, j)][sudoku[i][j]] = true;
+				check_3x3[get33Index(i,j)][sudoku[i][j]] = true;
 			}
 		}
 	}
@@ -63,12 +65,17 @@ int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	vector<vector<int>> sudoku(MAX, vector<int>(MAX));
+	//입력
 	for (int i = 0; i < MAX; i++) {
 		for (int j = 0; j < MAX; j++) {
 			cin >> sudoku[i][j];
 		}
 	}
+
+	//연산
 	auto output = solution(sudoku);
+
+	//출력
 	for (int i = 0; i < MAX; i++) {
 		for (int j = 0; j < MAX; j++) {
 			cout << output[i][j] << ' ';
