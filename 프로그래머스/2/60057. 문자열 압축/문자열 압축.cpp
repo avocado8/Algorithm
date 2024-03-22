a16b0b0908f3c975ac2a2e972221d8800f25ca02
avocado8 <95484237+avocado8@users.noreply.github.com>
2024-03-23 02:31:37 +0900

    [level 2] Title: 문자열 압축, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.cpp
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.cpp
@@ -3,37 +3,39 @@
 
 using namespace std;
 
-int getShortest(string target, int unit_length) {
-	int length = target.length();
-	int new_len = length;
-	int left = 0, right = unit_length;
-	string prev_str = "";
-	int cnt = 1;
-	while (right <= length) {
-		string curr_str = target.substr(left, unit_length);
-		if (prev_str == curr_str) cnt++;
-		else {
-			if (cnt > 1) { //이미 압축됨
-				new_len += to_string(cnt).length();
-				new_len -= (cnt - 1) * unit_length;
-			}
-			prev_str = curr_str;
-			cnt = 1;
-		}
-		left += unit_length;
-		right += unit_length;
-	}
-	if (cnt > 1) {
-		new_len += to_string(cnt).length();
-		new_len -= (cnt - 1) * unit_length;
-	}
-	return new_len;
+int getShortest(string target, int unit_length){
+    int length = target.length();
+    int new_len = length;
+    int cnt = 1;
+    int left = 0, right = unit_length;
+    string prev_str ="";
+    while(right <= length){
+        string curr_str = target.substr(left, unit_length);
+        if(prev_str == curr_str){
+            cnt++;
+        }
+        else {
+            if (cnt > 1){
+                new_len += to_string(cnt).length();
+                new_len -= (cnt-1)*unit_length;
+            }
+            prev_str = curr_str;
+            cnt = 1;
+        }
+        left += unit_length;
+        right += unit_length;
+    }
+    if (cnt > 1){
+        new_len += to_string(cnt).length();
+        new_len -= (cnt-1)*unit_length;
+    }
+    return new_len;
 }
 
-int solution(string str) {
-	int ans = str.length();
-	for (int i = 1; i <= str.length() / 2; i++) {
-		ans = min(ans, getShortest(str, i));
-	}
-	return ans;
+int solution(string s) {
+    int answer = s.length();
+    for (int i=1;i<=s.length()/2;i++){
+        answer = min(answer, getShortest(s, i));
+    }
+    return answer;
 }
