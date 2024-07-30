1397fd70b5b3cc2bdd7c1ebe7fa2a50db7baa582
avocado8 <95484237+avocado8@users.noreply.github.com>
2024-07-30 13:46:32 +0900

    [Silver V] Title: 집합, Time: 728 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.cc
+++ 백준/Silver/11723. 집합/집합.cc
@@ -1,81 +1,79 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-
 
 using namespace std;
-int s = 0;
 
-void add(int n) {
-	//추가: 해당 인덱스를 1로 표기
-	s = s | (1 << n);
-}
-void remove(int n) {
-	//삭제: 해당 인덱스를 0으로 표기
-	//1을 0으로 바꿔야하니까 ~(1<<n)과 and
-	s = s & ~(1 << n);
-}
+/*
+11723 집합
+1~20의 정수를 중복 없이 저장할 수 있는 집합
+비트의 위치로 해당 수가 집합에 있는지 여부를 판단
+*/
 
-int check(int n) {
-	//있음 : 해당 인덱스가 1, 없음: 0
-	if (s & (1 << n)) {
-		return 1;
-	}
-	else return 0;
+void all(int &s){
+    //모든 비트를 1로 만듦 (2^21 - 1)
+    s = (1 << 21) - 1;
 }
 
-void toggle(int n) {
-	if (s & (1 << n)) {
-		remove(n);
-	}
-	else add(n);
+void empty(int &s){
+    s = 0;
 }
 
-void all(){
-	s = (1 << 21) - 1;
+void add(int &s, int x){
+    s |= (1 << x); //x에 해당하는 비트를 1로 바꿈
 }
 
-void empty() {
-	s = 0;
+void remove(int &s, int x){
+    s &= ~(1 << x); //x에 해당하는 비트를 0으로 바꿈
 }
 
-int solution(string op, int x) {
-	if (op == "add") {
-		add(x);
-	}
-	else if (op == "remove") {
-		remove(x);
-	}
-	else if (op == "check") {
-		return check(x);
-	}
-	else if (op == "toggle") {
-		toggle(x);
-	}
-	return -1;
+int check(int &s, int x){
+    if (s & (1 << x)){
+        return 1;
+    }
+    else {
+        return 0;
+    }
 }
 
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	int m;
-	cin >> m;
-	for (int i = 0; i < m;i++) {
-		string op;
-		cin >> op;
-		if (op == "all") {
-			all();
-		}
-		else if (op == "empty") {
-			empty();
-		}
-		else {
-			int x;
-			cin >> x;
-			int ans = solution(op, x);
-			if (ans != -1) cout << ans << "\n";
-		}
-	}
+void toggle(int &s, int x){
+    s ^= (1 << x); //1이면 0으로, 0이면 1로 -> xor
+}
 
-	return 0;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int m, x, s = 0;
+    string op;
+    cin >> m;
+    
+    while(m--){
+        cin >> op;
+        
+        //피연산자가 입력되지 않는 연산: all, empty 
+        if(op=="all") {
+            all(s);
+        }
+        else if(op=="empty"){
+            empty(s);
+        }
+        
+        //피연산자가 입력되는 연산: add, remove, check, toggle
+        else {
+            cin >> x;
+            if(op=="add"){
+                add(s, x);
+            }
+            else if(op=="remove"){
+                remove(s, x);
+            }
+            else if(op=="check"){
+                cout << check(s, x) << '\n';
+            }
+            else if(op=="toggle"){
+                toggle(s, x);
+            }
+        }
+    }
+    
+    return 0;
 }
