0c6411fee44e406b1815cccde57d0559a0b85553
avocado8 <95484237+avocado8@users.noreply.github.com>
2024-08-20 13:03:10 +0900

    [Silver III] Title: 킹, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1063. 킹/킹.cc
+++ 백준/Silver/1063. 킹/킹.cc
@@ -1,50 +1,80 @@
 #include <iostream>
+#include <string>
 #include <vector>
-#include <algorithm>
-#include <map>
 
 using namespace std;
-typedef pair<int, int>ct;
+typedef pair<char, char> cc;
 
-void chess(string king, string stone, vector<string>op, int n) {
-	pair<int, int> k = { king[0], king[1]};
-	pair<int, int> s = { stone[0], stone[1]};
-	map<string, ct>move = { {"R",{1,0}}, {"L",{-1,0}}, {"B",{0,-1}}, {"T",{0,1}},
-	{"RT",{1,1}}, {"LT",{-1,1}}, {"RB",{1,-1}}, {"LB",{-1,-1}} };
-	for (int i = 0; i < n; i++) {
-		ct cur_move = move[op[i]]; //1,0과 같은 pair
-		int k_next_x = k.first + cur_move.first;
-		int k_next_y = k.second + cur_move.second;
-		if (k_next_x <= 72 && k_next_y <= 56 && k_next_x > 64 && k_next_y > 48) {
-			//킹 이동위치 = 돌 위치
-			if (k_next_x == s.first && k_next_y == s.second) {
-				int s_next_x = s.first + cur_move.first;
-				int s_next_y = s.second + cur_move.second;
-				if (s_next_x <= 72 && s_next_y <= 56 && s_next_x > 64 && s_next_y > 48) {
-					k = { k_next_x, k_next_y };
-					s = { s_next_x, s_next_y };
-				}
-			}
-			else {
-				k = { k_next_x, k_next_y };
-			}
-		}
-	}
-	cout << char(k.first) << char(k.second) << "\n";
-	cout << char(s.first) << char(s.second);
+/**
+ * input에 따라 이동한다.
+*/
+cc move(string input, char x, char y) {
+    for (char how : input) {
+        if (how == 'R') {
+            x++;
+        } else if (how == 'L') {
+            x--;
+        } else if (how == 'B') {
+            y--;
+        } else {  // (how == 'T')
+            y++;
+        }
+    }
+    return {x, y};
+}
+
+/**
+ * 두 좌표의 위치가 일치하는지 확인한다.
+*/
+bool isSame(cc k, cc s) {
+    return (k.first == s.first && k.second == s.second);
+}
+
+/**
+ * 체스판 범위를 넘어서는지 확인한다.
+*/
+bool checkRange (cc pos) {
+    if (pos.first < 'A' || pos.first > 'H' || pos.second < '1' || pos.second > '8') {
+        return false;
+    }
+    return true;
 }
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	int n;
-	string king, stone;
-	cin >> king >> stone >> n;
-	vector<string>op(n);
-	for (int i = 0; i < n; i++) {
-		cin >> op[i];
-	}
-	chess(king, stone, op, n);
+    cc k, s;  // king, stone 위치
+    int n;
+    string input;
 
-	return 0;
+    // 입력
+    cin >> k.first >> k.second >> s.first >> s.second >> n;
+
+    // 연산
+    while (n--) {
+        cin >> input;
+
+        cc next_k, next_s;
+
+        // king 이동
+        next_k = move(input, k.first, k.second);
+
+        // stone 이동
+        if (isSame(next_k, s)) {  // 이동한 king과 stone 위치가 일치하면
+            // king이 움직인 방향과 같은 방향으로 이동
+            next_s = move(input, s.first, s.second);
+        }
+        else {
+            next_s = s;
+        }
+
+        // 체스판 밖으로 나가지 않을 경우만 이동
+        if (checkRange(next_k) && checkRange(next_s)) {
+            k = next_k;
+            s = next_s;
+        }
+    }
+
+    // 출력
+    cout << k.first << k.second << '\n' << s.first << s.second;
+
+    return 0;
 }
