48e548e80d869c54e8aeb4a2a2c74cc1a6ac70f4
YeonHyeong Khil <95484237+avocado8@users.noreply.github.com>
2024-09-02 14:01:47 +0900

    [Bronze I] Title: 배고파(Easy), Time: 260 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/28239. 배고파（Easy）/배고파（Easy）.cc
+++ 백준/Bronze/28239. 배고파（Easy）/배고파（Easy）.cc
@@ -1,49 +1,47 @@
-// 1091: 배고파(Easy) (브론즈1/비트마스킹)
 #include <iostream>
-
 using namespace std;
-
 typedef long long ll;
 const int MAX = 60;
 
-pair<int, int> getPair(ll m) {
-    int x, y;
+pair<int,int> getPair(ll m){
+    int x, y; // x <= y
     
-    // m이 2의 제곱수인 경우 (m == 2^i)
-    // m = 2^(i-1) + 2^(i-1) (x == y == i)
-    for (ll i = 1; i < MAX; i++) {
-        if (m == ((ll)1 << i)) {
-            return {i - 1, i - 1};
+    // m이 2의 제곱수인 경우(m = 2^i)
+    // m=2^(i-1) + 2^(i-1)
+    // x=y=i-1
+    for(int i=0;i<MAX;i++){
+        // m == 2^i == 1<<i
+        if (m == ((ll)1 << i)){ //오버플로우 방지
+            return { i-1, i-1 };
         }
     }
     
     // m이 2의 제곱수가 아닌 경우
-    // m = 2^x + 2^y (x < y)
-    for (x = 0; x < MAX; x++) {
-        if (m & ((ll)1 << x)) {
-            m -= (ll)1 << x;
+    // m = 2^x + 2^y
+    for(x=0;x<MAX;x++){
+        //x번째 비트에 1이 저장되어 있다면 그 위치가 x
+        if (m & ((ll)1<<x)){
+            //=m의 x번째비트가 1이라면
+            //이 위치가 곧 x
             break;
         }
     }
-
-    for (y = x + 1; y < MAX; y++) {
-        if (m & ((ll)1 << y)) {
+    for(y=x+1;y<MAX;y++){
+        if (m & ((ll)1<<y)){
             break;
         }
     }
-    return {x, y};
+    return {x,y};
 }
 
-int main() {
+int main(){
     int n;
     ll m;
-    
     cin >> n;
-    while (n--) {
+    while(n--){
         cin >> m;
-        pair<int, int> ans = getPair(m);
+        pair<int, int > ans = getPair(m);
         cout << ans.first << " " << ans.second << "\n";
     }
-    
     return 0;
 }
