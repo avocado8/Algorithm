98586de6d4e2c0d2468424530e75d2c87269d9e4
YeonHyeong Khil <95484237+avocado8@users.noreply.github.com>
2024-09-05 22:57:21 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -6,39 +6,36 @@ using namespace std;
 const int CNT = 9;
 
 void solution(vector<int> &arr){
-    // 아홉 난쟁이의 키의 합
-    int sum =0;
+    int sum = 0; // 9명 난쟁이 키의 합
     for(int i=0;i<CNT;i++){
         sum += arr[i];
     }
-    
     for(int i=0;i<CNT;i++){
         for(int j=i+1;j<CNT;j++){
             if(sum - arr[i] - arr[j] == 100){
-                //둘을 뺸 값이 100이라면 이 둘이 잘못된난쟁이4
                 arr.erase(arr.begin() + j);
-                arr.erase(arr.begin() + i);
+                arr.erase(arr.begin() +i);
                 return;
             }
         }
     }
-    
 }
 
 int main(){
-    //입력
+    // 입력
     vector<int> arr(CNT);
     for(int i=0;i<CNT;i++){
         cin >> arr[i];
     }
     
-    //연산
+    // 연산
     solution(arr);
     sort(arr.begin(), arr.end());
     
-    //출력
+    // 출력
     for(auto it: arr){
         cout << it << '\n';
     }
+    
     return 0;
 }
