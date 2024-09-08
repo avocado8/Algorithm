ff2bf5a1aa1ce04587d4e7df7a40904b394cd4e9
YeonHyeong Khil <95484237+avocado8@users.noreply.github.com>
2024-09-08 14:32:54 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 44 ms, Memory: 6824 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.cc
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.cc
@@ -1,52 +1,67 @@
 #include <iostream>
 #include <vector>
-
-#define MAX 1000000
+#include <cmath>
 
 using namespace std;
 
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+#define MAX 1000000
 
-	vector<int> arr(MAX + 1, 0);
+int prime_num[MAX + 1]; //소수 여부 확인 (0: 소수 아님, 1: 소수)
+vector<int> prime; //소수 목록 저장
 
-	for (int i = 2; i <= MAX; i++) {
-        	arr[i] = i;
-    	}
+void primeCheck(){
+    //2부터 MAX까지 소수라고 가정
+    for(int i = 2; i <= MAX; i++){
+        prime_num[i] = i;
+    }
 
-    	for (int i = 2; i * i <= MAX; i++) {
-        	if (arr[i] == 0) {
-            		continue;
-        	}
-        	for (int j = i * i; j <= MAX; j += i) {
-            		arr[j] = 0;
-        	}
-    	}
-    	vector<int> odd_prime;
-    	for (int i = 3; i <= MAX; i++) {
-        	if (arr[i] != 0) {
-            		odd_prime.push_back(arr[i]);
-        	}
-    	}
+    for(int i = 2; i <= sqrt(MAX); i++){
+        //현재 숫자가 소수라면
+        if(prime_num[i] != 0){
+            //i의 배수들 소수 목록에서 제거
+            for(int j = i * i; j <= MAX; j += i){
+                prime_num[j] = 0; // j는 소수가 아님
+            }
+        }
+    }
 
-    	while (true) {
-        	int n;
-        	cin >> n;
+    //prime_num에서 소수들만 prime 벡터에 저장
+    for(int i = 2; i <= MAX; i++){
+        if(prime_num[i] != 0){
+            prime.push_back(prime_num[i]);
+        }
+    }
+}
 
-        	if (n == 0) {
-            		break;
-        	}
+int main(){
 
-        	for (int i = 0; i < odd_prime.size(); i++) {
-            		if (arr[n - odd_prime[i]] == n - odd_prime[i]) {
-                		cout << n << " = " << odd_prime[i] << " + " << n - odd_prime[i] << '\n';
-                		break;
-            		}
-        	}
-    	}
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);  cout.tie(NULL);
 
-	return 0;
+    primeCheck();
+    int test_n;
+
+    while(1){
+        cin >> test_n;
+        if(test_n == 0){
+            break;
+        }
+
+        bool flag = false; //추측이 맞는지 여부
+
+        //소수 목록 탐색 -> 소수 합이 test_n이 되는지 확인
+        for(int i = 0; i < prime.size(); i++){
+            // test_n - prime[i]가 소수인지 확인
+            if(prime_num[test_n - prime[i]] == test_n - prime[i]){
+                cout << test_n << " = " << prime[i] << " + " << test_n - prime[i] << '\n';
+                flag = true;
+                break;
+            }
+        }
+
+        if(!flag){
+            cout << "Goldbach's conjecture is wrong.\n";
+        }
+    }
+    return 0;
 }
