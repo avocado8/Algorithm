e3d5eb0878a69eeafcb0150a4642beaf4e206b8f
YeonHyeong Khil <95484237+avocado8@users.noreply.github.com>
2024-09-08 14:21:28 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 52 ms, Memory: 6824 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.cc
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.cc
@@ -1,63 +1,52 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
-#include <algorithm>
+
+#define MAX 1000000
 
 using namespace std;
 
-// 전역 변수
-const int MAX_LIMIT = 1000000;
-vector<bool> is_prime(MAX_LIMIT + 1, true);
-vector<int> primes;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-// 에라토스테네스의 체를 사용하여 전체 소수 목록 생성
-void erato() {
-    is_prime[0] = is_prime[1] = false;
+	vector<int> arr(MAX + 1, 0);
 
-    for (int i = 2; i <= sqrt(MAX_LIMIT); i++) {
-        if (is_prime[i]) {
-            for (int j = i * i; j <= MAX_LIMIT; j += i) {
-                is_prime[j] = false;
-            }
-        }
-    }
+	for (int i = 2; i <= MAX; i++) {
+        	arr[i] = i;
+    	}
 
-    // 소수 목록을 벡터에 추가
-    for (int i = 3; i <= MAX_LIMIT; i += 2) { // 홀수 소수만 추가한다. 
-        if (is_prime[i]) {
-            primes.push_back(i);
-        }
-    }
-}
+    	for (int i = 2; i * i <= MAX; i++) {
+        	if (arr[i] == 0) {
+            		continue;
+        	}
+        	for (int j = i * i; j <= MAX; j += i) {
+            		arr[j] = 0;
+        	}
+    	}
+    	vector<int> odd_prime;
+    	for (int i = 3; i <= MAX; i++) {
+        	if (arr[i] != 0) {
+            		odd_prime.push_back(arr[i]);
+        	}
+    	}
 
-// 두 소수의 합으로 n을 나타낼 수 있는지 확인
-string find_goldbach(int n) {
-    for (int prime : primes) {
-        if (prime > n / 2) break; // a가 n/2보다 크면 끝낸다. 
+    	while (true) {
+        	int n;
+        	cin >> n;
 
-        int b = n - prime;
-        if (is_prime[b]) { // b가 소수인 경우에, 즉 찾았다. 
-            return to_string(n) + " = " + to_string(prime) + " + " + to_string(b);
-        }
-    }
+        	if (n == 0) {
+            		break;
+        	}
 
-    return "Goldbach's conjecture is wrong.";
-}
+        	for (int i = 0; i < odd_prime.size(); i++) {
+            		if (arr[n - odd_prime[i]] == n - odd_prime[i]) {
+                		cout << n << " = " << odd_prime[i] << " + " << n - odd_prime[i] << '\n';
+                		break;
+            		}
+        	}
+    	}
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    erato(); // 소수 목록을 미리 계산
-
-    while (true) {
-        int n;
-        cin >> n;
-
-        if (n == 0) break;
-
-        cout << find_goldbach(n) << "\n";
-    }
-
-    return 0;
+	return 0;
 }
