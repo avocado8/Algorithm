e2bfeae71be820298dd50e9cff749fba31d836d3
YeonHyeong Khil <95484237+avocado8@users.noreply.github.com>
2024-09-08 13:35:08 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 56 ms, Memory: 3044 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.cc
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.cc
@@ -1,64 +1,63 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
+#include <algorithm>
 
 using namespace std;
 
-const int MAX = 1000000;    //n의 최대 범위
-vector<int> primes;         //소수 저장 벡터
-vector<bool> is_prime(MAX + 1, true);   //소수 여부 판별
+// 전역 변수
+const int MAX_LIMIT = 1000000;
+vector<bool> is_prime(MAX_LIMIT + 1, true);
+vector<int> primes;
 
-
-//최대범위 내의 소수 구하기
-void getPrimes() {
+// 에라토스테네스의 체를 사용하여 전체 소수 목록 생성
+void erato() {
     is_prime[0] = is_prime[1] = false;
-    for(int i = 2; i * i <= MAX; i++) {
-        if(is_prime[i]) {  //소수이면 벡터에 저장
-            primes.push_back(i);
 
-            for(int j = i * i; j <= MAX; j += i) {    //배수는 전부 제외
+    for (int i = 2; i <= sqrt(MAX_LIMIT); i++) {
+        if (is_prime[i]) {
+            for (int j = i * i; j <= MAX_LIMIT; j += i) {
                 is_prime[j] = false;
             }
         }
     }
-}
 
-//b-a가 가장 큰 쌍 구하기
-int findMax(int n) {
-    for(int i = 0; i < primes.size(); i++) {
-        int a = primes[i];
-        int b = n - a;
-        if(is_prime[b]) {
-            return a;
+    // 소수 목록을 벡터에 추가
+    for (int i = 3; i <= MAX_LIMIT; i += 2) { // 홀수 소수만 추가한다. 
+        if (is_prime[i]) {
+            primes.push_back(i);
         }
     }
-    return -1;
 }
 
+// 두 소수의 합으로 n을 나타낼 수 있는지 확인
+string find_goldbach(int n) {
+    for (int prime : primes) {
+        if (prime > n / 2) break; // a가 n/2보다 크면 끝낸다. 
 
+        int b = n - prime;
+        if (is_prime[b]) { // b가 소수인 경우에, 즉 찾았다. 
+            return to_string(n) + " = " + to_string(prime) + " + " + to_string(b);
+        }
+    }
+
+    return "Goldbach's conjecture is wrong.";
+}
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    cout.tie(NULL);
 
-    getPrimes();
-    int n;
+    erato(); // 소수 목록을 미리 계산
 
-    while(true) {
-        //입력
+    while (true) {
+        int n;
         cin >> n;
-        if(n == 0) break;
 
-        //출력
-        int a = findMax(n);
-        if(a != -1) {
-            cout << n << " = " << a << " + " << n - a << '\n';
-        }
-        else {
-            cout << "Goldbach's conjecture is wrong.\n";
-        }
+        if (n == 0) break;
 
+        cout << find_goldbach(n) << "\n";
     }
-    return 0;
 
+    return 0;
 }
