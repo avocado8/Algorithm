ba56d19bd60cf958546e3063e136d57849c47a1e
YeonHyeong Khil <95484237+avocado8@users.noreply.github.com>
2024-09-10 15:34:14 +0900

    [Gold V] Title: 물병, Time: 152 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1052. 물병/물병.cc
+++ 백준/Gold/1052. 물병/물병.cc
@@ -1,9 +1,9 @@
 #include <iostream>
-
 using namespace std;
 const int MAX = 25;
 
 int countOne(int n){
+    //1 개수 세기
     int cnt = 0;
     for(int i=0;i<MAX;i++){
         if(n & (1 << i)){
@@ -14,20 +14,20 @@ int countOne(int n){
 }
 
 int solution(int n, int k){
+    
+    // k개 안에 담을 수 있는지 확인
+    // 없다면 물병수 늘려서 다시 확인...
     int answer = 0;
     while(true){
-        if(countOne(n+answer) <= k){
-            return answer;
-        }
+        if(countOne(n+answer) <= k) return answer;
         answer++;
     }
-    return -1;
+    
 }
 
 int main(){
-    int n, k;
+    int n,k;
     cin >> n >> k;
-    
     cout << solution(n, k);
-    
+    return 0;
 }
