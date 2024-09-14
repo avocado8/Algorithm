42e232fd4cf3f65ab67b26147f1e49a094d46477
YeonHyeong Khil <95484237+avocado8@users.noreply.github.com>
2024-09-14 11:41:04 +0900

    [Silver V] Title: 집합, Time: 556 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.cc
+++ 백준/Silver/11723. 집합/집합.cc
@@ -1,75 +1,51 @@
-// 집합
-// 비트마스크
 #include <iostream>
-#include <string>
+
 using namespace std;
 
-void all(int &s){   // s의 1부터 20까지의 인덱스의 비트 필드를 1로 채운다.
-    s = (1 << 21) - 1;
-}
-void empty(int &s){ // s의 1부터 20까지의 인덱스의 비트 필드가 0이 되도록 한다.
-    s = 0;
-}
-void add(int &s, int x){    // s의 인덱스 x인 비트 필드를 1로 만든다.
-    int temp = 1 << x;
-    s = s | temp;   // or 연산
-}
-void remove(int &s, int x){    // s의 인덱스 x인 비트 필드를 0으로 만든다.
-    int temp = 1 << x;
-    s = s & (~temp);   // a-b = a&(~b)
-}
-int check(int &s, int x){    // s의 인덱스 x인 비트 필드가 0이면 0, 1이면 1을 반환한다.
-    int temp = 1 << x;
-    int answ = (s & temp)? 1: 0;
-    return answ;
-}
-void toggle(int &s, int x){   
-    if (check(s,x)){  // s의 인덱스 x인 비트 필드가 0이면 1로 만든다.
-        remove(s,x);
-    }
-    else {  // s의 인덱스 x인 비트 필드가 1이면 0으로 만든다.
-        add(s,x);
-    }
-}
+int main() {
+    ios::sync_with_stdio(false); 
+    cin.tie(NULL);
 
-int main(){
-    // 입출력 속도 향상
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);  cout.tie(NULL);
+    int M;
+    cin >> M;
 
-    int s = 0;  // s: 1부터 20까지 인덱스의 비트필드를 사용 
-    int m;  // m: 수행해야 하는 연산의 수
-    cin >> m;
+    int n = 0;
 
-    while (m--){
-        string input;   // input: 입력받은 수행해야 하는 명령어
-        cin >> input;
+    while(M--) {
+        string s;
+        int x;
+        cin >> s;
 
-        if (input == "all"){ // S를 {1, 2, ..., 20} 으로 바꾼다.
-            all(s); 
-            continue;
+        if(s=="add") {
+            cin >> x;
+            n |= (1 << (x-1));
         }
-        else if (input == "empty"){ // S를 공집합으로 바꾼다.
-            empty(s);   
-            continue;
+        else if(s=="remove") {
+            cin >> x;
+            n &= ~(1 << (x-1));
         }
-
-        int x;  // x: 명령어가 all, empty일 때 빼고는 필요함. (x는 1이상 20이하)
-        cin >> x;
-
-        if (input == "add"){ // S에 x를 추가한다. x가 이미 있는 경우에는 연산을 무시한다.
-            add(s,x);   
+        else if(s=="check") {
+            cin >> x;
+            if(n & (1 << (x-1))) {
+                cout << 1 << '\n';
+            }
+            else cout << 0 << '\n';
         }
-        else if (input == "remove"){ // S에서 x를 제거한다. S에 x가 없는 경우에는 연산을 무시한다.
-            remove(s,x);    
+        else if(s=="toggle") {
+            cin >> x;
+            if(n & (1 << (x-1))) {
+                n &= ~(1 << (x-1));
+            }
+            else {
+                n |= (1 << (x-1));
+            }
         }
-        else if (input == "check"){ // S에 x가 있으면 1을, 없으면 0을 출력한다.
-            cout << check(s,x) << '\n';
+        else if(s=="all") {
+             n = (1 << 20) - 1;
         }
-        else if (input == "toggle"){ // S에 x가 있으면 x를 제거하고, 없으면 x를 추가한다.
-            toggle(s,x);
+        else if(s=="empty") {
+            n = 0;
         }
     }
-
     return 0;
 }
