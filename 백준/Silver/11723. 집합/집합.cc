b66f71beab0695b041ca367a2fb8a6ec4a0779a5
YeonHyeong Khil <95484237+avocado8@users.noreply.github.com>
2024-09-14 12:13:17 +0900

    [Silver V] Title: 집합, Time: 512 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.cc
+++ 백준/Silver/11723. 집합/집합.cc
@@ -1,51 +1,82 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
+int calculate(int& s, string operation) {
+
+	int x = 0;
+	int pos = operation.find(" "); //���� �������� ������ ����
+	string oper = operation.substr(0, pos); // ���� ����
+
+	if (pos != string::npos) { // ������ ������ ���� ����(*������ ã�� ���ϸ� npos ��ȯ)
+		x = stoi(operation.substr(pos + 1)); // ���� ������ ����, string -> int, x��
+	}
+
+	int x_bit = 1 << x; //x�� ��Ʈ����ũ�� ǥ��
+
+	if (oper == "add") {
+		s |= x_bit; //x �߰�, or ����(������)
+	}
+	else if (oper == "remove") {
+		s &= ~x_bit; //x ����, ������
+	}
+	else if (oper == "check") {
+		if ((s & x_bit) == x_bit) { //s�� x�� and ����(������)�� x���(== s�� x�� �ִٸ�) 1 ��ȯ
+			return 1;
+		}
+		else { //x�� ���ٸ� 0 ��ȯ
+			return 0;
+		}
+	}
+	else if (oper == "toggle") {
+		if ((s & x_bit) == x_bit) { //s�� x�� and ����(������)�� x���(== s�� x�� �ִٸ�)
+			s &= ~x_bit; //x ����
+		}
+		else { //x�� ���ٸ�
+			s |= x_bit; //x �߰�
+		}
+	}
+	else if (oper == "all") {
+		s = 0; //���� ���� ��
+		for (int i = 1; i <= 20; i++) { //1���� 20���� �߰�
+			int tmp_bit = 1 << i;
+			s |= tmp_bit; //������
+		}
+	}
+	else if (oper == "empty") {
+		s = 0; //���� ����(== s�� ����������)
+	}
+
+	return 2; //check�� �ƴϸ� 2 ��ȯ
+}
+
 int main() {
-    ios::sync_with_stdio(false); 
-    cin.tie(NULL);
 
-    int M;
-    cin >> M;
+	//����� �ӵ� ���
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    int n = 0;
+	int m;
+	string operation;
+	int s = 0;
+	int check_result;
 
-    while(M--) {
-        string s;
-        int x;
-        cin >> s;
+	//�Է� & ����
+	cin >> m;
+	cin.ignore(); //���� ���� ����
 
-        if(s=="add") {
-            cin >> x;
-            n |= (1 << (x-1));
-        }
-        else if(s=="remove") {
-            cin >> x;
-            n &= ~(1 << (x-1));
-        }
-        else if(s=="check") {
-            cin >> x;
-            if(n & (1 << (x-1))) {
-                cout << 1 << '\n';
-            }
-            else cout << 0 << '\n';
-        }
-        else if(s=="toggle") {
-            cin >> x;
-            if(n & (1 << (x-1))) {
-                n &= ~(1 << (x-1));
-            }
-            else {
-                n |= (1 << (x-1));
-            }
-        }
-        else if(s=="all") {
-             n = (1 << 20) - 1;
-        }
-        else if(s=="empty") {
-            n = 0;
-        }
-    }
-    return 0;
+	for (int i = 0; i < m; i++) {
+		getline(cin, operation); //���� ���� �Է�
+		check_result = calculate(s, operation);
+
+		//���
+		if (check_result == 0 || check_result == 1) { //check_result�� 0 �Ǵ� 1�̸�(== check �����̸�)
+			cout << check_result << "\n";
+		}
+	}
+
+	return 0;
 }
