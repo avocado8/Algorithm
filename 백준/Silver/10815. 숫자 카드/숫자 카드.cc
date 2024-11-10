d18b1ec8cb6d9677920366e87152b14a5bdacbf5
YeonHyeong Khil <95484237+avocado8@users.noreply.github.com>
2024-11-10 23:20:52 +0900

    [Silver V] Title: 숫자 카드, Time: 196 ms, Memory: 3976 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.cc
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -4,57 +4,38 @@
 
 using namespace std;
 
-// 해당 수의 카드가 몇 개인지 세는 함수
-int countCards(vector<int>& nums, int target) {
-	int count = 0;
-
-	int le = 0, ri = nums.size() - 1;
-
-	while (le <= ri) {
-		int mid = (le + ri) / 2;
-
-		// target 값을 찾은 경우
-		if (nums[mid] == target) {
-			return 1;
-		}
-
-		if (nums[mid] > target) {
-			ri = mid - 1;
-		}
-
-		else if (nums[mid] < target) {
-			le = mid + 1;
-		}
-	}
-
-	// target이 존재하지 않는 경우, 0 반환
-	return 0;
+bool existsInCards(const vector<int>& cards, int num) {
+    //카드 목록에 num이 있는지 확인
+    return binary_search(cards.begin(), cards.end(), num);
 }
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-	int n, m;
-	int target;
+    int n, m, num;
 
-	cin >> n;
-	vector<int> nums(n);
+    cin >> n;
 
-	for (int i = 0; i < n; i++) {
-		cin >> nums[i];
-	}
+    vector<int> cards(n);
+    for (int i = 0; i < n; i++) {
+        cin >> cards[i];
+    }
 
-	// 이분 탐색을 위한 정렬
-	sort(nums.begin(), nums.end());
+    sort(cards.begin(), cards.end());
 
-	cin >> m;
+    cin >> m;
 
+    for (int i = 0; i < m; i++) {
+        cin >> num;
 
-	for (int i = 0; i < m; i++) {
-		cin >> target;
-		cout << countCards(nums, target) << ' ';
-	}
+        if (existsInCards(cards, num)) {
+            cout << '1' << ' ';
+        } else {
+            cout << '0' << ' ';
+        }
+    }
 
-	return 0;
+    return 0;
 }
