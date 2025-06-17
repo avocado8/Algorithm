3f7d8dc072b90cc82cc6d23222ad40b27924ece6
YeonHyeong Khil <95484237+avocado8@users.noreply.github.com>
2025-06-17 17:37:52 +0900

    [level 2] Title: 튜플, Time: 29.99 ms, Memory: 42.9 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -1,12 +1,11 @@
 function solution(s) {
     const answer = [];
     let arr_s = s.slice(2, -2).split("},{").map((el)=>el.split(',').map(Number)).sort((a,b)=>a.length-b.length)
-    console.log(arr_s)
-    let prev = [];
+    let seen = new Set();
     for(let i=0;i<arr_s.length;i++){
-        let new_num = arr_s[i].filter((el)=>!prev.includes(el));
-        answer.push(new_num[0]);
-        prev = arr_s[i];
+        let next = arr_s[i].find(num => !seen.has(num));
+        answer.push(next);
+        seen.add(next);
     }
     return answer;
 }
